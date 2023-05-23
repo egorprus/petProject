b461176c10b671e6f33f38df9e8972fdd654690e
egorprus <prus.egor.nik@gmail.com>
2023-05-23 13:07:14 +0300

    refactoring

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from 'mongoose';
+import multer from 'multer';
+import cors from 'cors';
 import { registerValidation, loginValidation, postCreateValidation } from './validations/validations.js';
-import checkAuth from './utils/checkAuth.js';
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+import { PostController, UserController } from "./controllers/index.js";
 
 mongoose
     .connect('mongodb://REDACTED:REDACTED@REDACTED-HOST:REDACTED')
@@ -12,16 +13,35 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename:(_, file, cb) => {
+        cb(null, file.originalname); 
+    }
+});
+
+const upload = multer({ storage })
+
 app.use(express.json());
+app.use(cors());
  
-app.post('/login', loginValidation, UserController.login);
-app.post('/register', registerValidation, UserController.register);
+app.post('/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.post('/posts', checkAuth, postCreateValidation,  PostController.create);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
+app.post('/upload', checkAuth, upload.single('image'), (req,res) => {
+    res.json({
+        url: `uploads/${req.file.originalname}`
+    })
+})
 app.listen(8080, () => {
     console.log('server ok')
 })
